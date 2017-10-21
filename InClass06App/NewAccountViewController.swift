a1c3dd6d17c2f1ab295067818a3e15bef1a91575
Akshay <aayyanch@uncc.edu>
2017-10-21 08:38:48 -0400

    NewAccountViewcontroller added with constraints and code to dismiss the screen

---
+++ InClass06App/NewAccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewAccountViewController.swift
+//  InClass06App
+//
+//  Created by Ayyanchira, Akshay Murari on 10/21/17.
+//  Copyright © 2017 Example. All rights reserved.
+//
+
+import UIKit
+
+class NewAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
